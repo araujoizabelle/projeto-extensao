0c3c177727a353cf411f45af467df6ab151e7829
Nattan Duarte <nattanfd@gmail.com>
2017-10-09 22:49:41 -0300

    páginas adicionais criadas ;)

---
+++ extensao/home.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+<head>
+	<title>3</title>
+</head>
+<body>
+	<header>
+		<h1>Nome</h1>
+		<button>Grade</button>
+		<button>Sair</button>
+	</header>
+	<main>
+		<table>
+			<tr>
+				<th></th>
+				<th>Título</th>
+				<th>Data</th>
+				<th>Hora</th>
+			</tr>
+			<tr>
+				<td>Ckeck</td>
+				<td>Curso de xml</td>
+				<td>09/02</td>
+				<td>14:00</td>
+			</tr>
+		</table>
+		<button>Salvar</button>
+	</main>
+</body>
+</html>
+
+essa linha que vc está fazendo nao precisa. Ela será gerada automaticamnete
+Eu sei, mas é só pra ter um guia, ele pediu pra mim hj kkk eu tinha dito "mas Escalf, nós vamos fazer só com php não precisa fazer agr". Aí ele pediu pra fazer uma estrutura assim com exemplo de como ia ficar
