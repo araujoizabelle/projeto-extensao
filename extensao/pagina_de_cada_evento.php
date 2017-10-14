41dadcf709e6450f576b62005294d93e2c715087
Nattan Duarte <nattanfd@gmail.com>
2017-10-14 12:12:34 -0300

    css atualizado (páginas individuais de eventos)

--- extensao/pagina_de_cada_evento.php
+++ extensao/pagina_de_cada_evento.php
@@ -4,24 +4,30 @@
 <head>
 	<title>Eventos</title>
 	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="./css/tipo_evento/evento.css">
-	<link rel="stylesheet" type="text/css" href="./css/tipo_evento/#.css"><!-- Aqui será colocada a folha de estilo do evento específico-->
+	<link rel="stylesheet" type="text/css" href="./tipo_evento/css/evento.css">
+	<link rel="stylesheet" type="text/css" href="./tipo_evento/css/#.css"> <!--Aqui será colocada a folha de estilo do evento específico-->
 	<link rel="stylesheet" href="./css/font-awesome-4.7.0/css/font-awesome.min.css">
+   <link href="https://fonts.googleapis.com/css?family=Monda:700" rel="stylesheet">
 	<script src="./js/jquery.min.js"></script>
 </head>
 <body>
 	<header class="cabecalho">
 		<h1>CEFET/RJ Nova Friburgo</h1>
 		<h1>Semana de Extensão 2017</h1>
-		<p>Fulano</p>
+		<p>Fulano de Tal</p>
 		<button class="logout">Sair</button>
 	</header>
 	<main class="main">
-		<section class="evento">
-			<h3>TIPO</h3>
+        <section class="evento">
+            <div class="tipo_evento">
+                <h3>tipo do evento</h3>
+            </div>
+		  <div class="info_evento">
 			<h2>NOME</h2>
 			<h3>SALA</h3>
-		</section>
+            <div class="buttonInscrever"><p>Inscrever-se</p></div>
+		  </div>
+        </section>
 		<div class="description">
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -29,18 +35,17 @@
 			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-			<div class="buttonInscrever"><p>Inscrever-se</p></div>
-		</div>
+        </div>	
 		<section class="palestrante">
 			<img src="./img/perfilpadrao.jpg" alt="Foto do palestrante"><!-- quando o palestrante não apresentar foto no lattes, deixar essa silhueta como padrão! -->
 			<article class="bioPalestrante">
-				<h3>Ciclano</h3>
+				<h3>Nome e Sobrenome</h3>
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 				consequat.</p><br/>
 				<h4><i class="fa fa-envelope-open-o" aria-hidden="true"></i> palestrante@email.com</h4>
-				<i class="fa fa-phone" aria-hidden="true"></i> (00) 0 0000-0000</h4>
+				<h4><i class="fa fa-phone" aria-hidden="true"></i> (00) 0 0000-0000</h4>
 			</article>
 		</section>
 		<section class="palestrante">
@@ -52,19 +57,19 @@
 				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 				consequat.</p><br/>
 				<h4><i class="fa fa-envelope-open-o" aria-hidden="true"></i> palestrante@email.com</h4>
-				<i class="fa fa-phone" aria-hidden="true"></i> (00) 0 0000-0000</h4>
+				<h4><i class="fa fa-phone" aria-hidden="true"></i> (00) 0 0000-0000</h4>
 			</article>
 		</section>
 		<section class="palestrante">
 			<img src="./img/perfilpadrao.jpg" alt="Foto do palestrante"><!-- quando o palestrante não apresentar foto no lattes, deixar essa silhueta como padrão! -->
 			<article class="bioPalestrante">
-				<h3>Ciclano</h3>
+				<h3>Nome e Sobrenome</h3>
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 				consequat.</p><br/>
 				<h4><i class="fa fa-envelope-open-o" aria-hidden="true"></i> palestrante@email.com</h4>
-				<i class="fa fa-phone" aria-hidden="true"></i> (00) 0 0000-0000</h4>
+				<h4><i class="fa fa-phone" aria-hidden="true"></i> (00) 0 0000-0000</h4>
 			</article>
 		</section>
 	</main>
